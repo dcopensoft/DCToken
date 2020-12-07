a3989d51b6b8b86b34f6cf97c0fe357439088f35
dcopensoft <75019329+dedcgroup@users.noreply.github.com>
2020-12-07 18:29:49 +0800

    Update MainActivity.cs

--- dedc.wallet/dedc.wallet.Android/MainActivity.cs
+++ dedc.wallet/dedc.wallet.Android/MainActivity.cs
@@ -16,9 +16,7 @@ namespace DCToken.Droid
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
             base.OnCreate(savedInstanceState);
-
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
